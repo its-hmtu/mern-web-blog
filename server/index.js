f08b67bcca6b476a937681e01d44f7c7234b7386
its-hmtu <tu.hoangminh15@gmail.com>
2024-05-21 10:51:02 +0700

    Update server to serve static files in production

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cors from 'cors'
@@ -22,7 +23,19 @@ app.use(cookieParser())
 
 app.use('/api/users', userRoutes)
 
-app.get("/", (req, res) => res.send("Server is ready!"))
+if (process.env.NODE_ENV === 'development') { // change to 'production' later
+  const __dirname = path.resolve()
+  app.use(express.static(path.join(__dirname, '/client/dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
