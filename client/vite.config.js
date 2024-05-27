0ab0d3630bc8326edb117b12a2ba5be518fa1ecd
its-hmtu <tu.hoangminh15@gmail.com>
2024-05-27 08:24:20 +0700

    add jsconfig for alias

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,14 @@ export default defineConfig({
   resolve: {
     alias: {
       src: "/src",
-      styles: '/src/assets/styles/'
-    }
-  }
+      components: "/src/components",
+      pages: "/src/pages",
+      assets: "/src/assets",
+      layout: "/src/layout",
+      utils: "/src/utils",
+      styles: "/src/assets/styles",
+      images: "/src/assets/images",
+      svg: "/src/assets/svg",
+    },
+  },
 })
