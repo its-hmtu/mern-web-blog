e358d5fb14320ed358a9196a5d650f08f1a593ad
TrungTo13803 <trungto13082003@gmail.com>
2024-06-21 20:45:20 +0700

    update UI dang nhap

--- client/src/pages/SignInPage/index.jsx
+++ client/src/pages/SignInPage/index.jsx
@@ -11,32 +11,39 @@ const SignInPage = () => {
     setShowPassword(!showPassword);
   }
 
-  // useEffect(() => {
-  //   ref.current.focus()
-  // }, [])
+  useEffect(() => {
+    ref.current.focus()
+  }, [])
 
   return (
     <div className='sign-in-wrapper'>
       <Container fluid className="sign-in-container">
-        <div className='logo-wrapper'>
-          <h1>DEV</h1>  
+        <div className='signin-content'>
+          <a href="" >
+            <img className='signin-logo' src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/original_logo_0DliJcfsTcciZen38gX9.png" alt="DEV Community" />
+          </a>
+          <h1 className='signin-title'>Join the DEV Community</h1>
+          <p className='signin-description'>DEV Community is a community of 1,625,044 amazing developers</p>
         </div>
         <div className="sign-in-container-inner">
           <Col sm={12}>
             <div className="sign-in-box">
-              <div className="text-start">
-                <h1 >Join the DEV Community</h1>
-                <p >DEV Community is a community of 1,625,044 amazing developers</p>
-              </div>
               <Button variant="light" className="login-button" block>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" role="img" aria-hidden="true" class="crayons-icon crayons-icon--default">
+                  <path d="M18.09 18.75c2.115-1.973 3.052-5.25 2.49-8.393h-8.392v3.473h4.777a3.945 3.945 0 0 1-1.777 2.67l2.902 2.25Z" fill="#4285F4"></path>
+                  <path d="M4.215 15.982A9 9 0 0 0 18.09 18.75l-2.902-2.25a5.37 5.37 0 0 1-8.018-2.813l-2.955 2.296Z" fill="#34A853"></path>
+                  <path d="M7.17 13.687c-.375-1.17-.375-2.25 0-3.42L4.215 7.965a9.06 9.06 0 0 0 0 8.025l2.955-2.303Z" fill="#FBBC02"></path>
+                  <path d="M7.17 10.267c1.035-3.24 5.438-5.115 8.393-2.347l2.58-2.528A8.85 8.85 0 0 0 4.215 7.965l2.955 2.302Z" fill="#EA4335"></path>
+                </svg>
                 Continue with Google
               </Button>
             
               <div className="separator-top">
                 <hr className="left-line" />
-                  <span>or</span>
+                  <span>OR</span>
                 <hr className="right-line" />
               </div>
+
               <Form>
                 <Form.Group>
                   <Form.Label>Email</Form.Label>
@@ -47,9 +54,6 @@ const SignInPage = () => {
                   <Form.Label>Password</Form.Label>
                   <InputGroup>
                     <Form.Control type={showPassword ? 'text' : 'password'} />
-                    {/* <Button variant="dark" onClick={handleShowPassword}>
-                      {showPassword ? 'Hide' : 'Show'}
-                    </Button> */}
                   </InputGroup>
                 </Form.Group>
 
