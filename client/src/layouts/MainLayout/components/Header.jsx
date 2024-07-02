d6fdaffef0f46cf79b502346b1aa962b435dbb2e
its-hmtu <tu.hoangminh15@gmail.com>
2024-07-02 09:38:34 +0700

    Update header + main layout UI

--- client/src/layouts/MainLayout/components/Header.jsx
+++ client/src/layouts/MainLayout/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Navbar, Nav, Container, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-regular-svg-icons";
+import { faBell as faBellSolid } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const [user, setUser] = React.useState(true);
+
   return (
     <header className="main-layout__header">
       <Navbar>
@@ -14,15 +19,49 @@ const Header = () => {
         </Navbar.Brand>
         <Nav className="me-auto">{/* {Search bar} */}</Nav>
 
-        <Nav>
-          <Link to="/signin" className="me-2">
-            <Button variant="outline-primary" className="main-layout__header-btn sign-in">Sign in</Button>
-          </Link>
+        {user ? (
+          <Nav>
+            <Row className="flex-row">
+              <Col className="d-flex align-items-center">
+                <Link to="/create-post" className="me-2">
+                  <Button
+                    variant="outline-primary"
+                    className="main-layout__header-btn"
+                  >
+                    Create Post
+                  </Button>
+                </Link>
 
-          <Link to="/register" className="ms-2">
-            <Button variant="primary" className="main-layout__header-btn">Create account</Button>
-          </Link>
-        </Nav>
+                <Link to="/notifications" className="ms-2 me-2">
+                  <FontAwesomeIcon icon={faBell} className="main-layout__header-noti"/>
+                </Link>
+
+                <Link to='/profile' className="ms-2">
+                  <div className="main-layout__header-user p-1">
+                    <img src="https://via.placeholder.com/150" alt="User" className="main-layout__header-user-img" />
+                  </div>
+                </Link>
+              </Col>
+            </Row>
+          </Nav>
+        ) : (
+          <Nav>
+            <Link to="/signin" className="me-2">
+              <Button
+                variant="outline-primary"
+                className="main-layout__header-btn sign-in"
+              >
+                Sign in
+              </Button>
+            </Link>
+
+            <Link to="/register" className="ms-2">
+              <Button variant="primary" className="main-layout__header-btn">
+                Create account
+              </Button>
+            </Link>
+          </Nav>
+        )}
       </Navbar>
     </header>
   );
--- client/src/layouts/MainLayout/index.jsx
+++ client/src/layouts/MainLayout/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
-import Footer from './components/Footer'
 
 const MainLayout = () => {
   return (
@@ -10,7 +9,6 @@ const MainLayout = () => {
       <div className="main-layout__outlet">
         <Outlet />
       </div>
-      <Footer />
     </div>
   )
 }
